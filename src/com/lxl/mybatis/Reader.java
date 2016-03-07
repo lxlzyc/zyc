6ef621ab3dbd18e69d9b3e9642ee3a597dcb9f8f
lxl <lxldyx001@126.com>
2016-03-07 21:36:01 +0800

    study mybatis

---
+++ src/com/lxl/mybatis/Reader.java
@@ -0,0 +1,26 @@
+package com.lxl.mybatis;
+
+public class Reader {
+	private int readerId;
+	private Integer userId;
+	private String money;
+	public int getReaderId() {
+		return readerId;
+	}
+	public void setReaderId(int readerId) {
+		this.readerId = readerId;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getMoney() {
+		return money;
+	}
+	public void setMoney(String money) {
+		this.money = money;
+	}
+	
+}
